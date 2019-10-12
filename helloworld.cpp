b0309d977d070bd057805df191fd307de3d0cdd8
liujiangyong <liujiangyong@liu-MacBook.local>
2019-10-12 15:00:22 +0800

    de

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{   for (int  i = 0; i < 10; i++)
+    cout<<"ni 你好 !"<<endl;
+    unsigned int y = 99;
+    //cin>>y;
+    cout<<y<<endl;
+     
+    return 0;
+}
